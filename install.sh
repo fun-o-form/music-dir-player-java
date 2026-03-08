525b8597b3ffeb64d03c284cb47c1109bf06bc4d
jonathanknez <jonathanknez@gmail.com>
2026-03-08 10:21:36 -0600

    installer working, bumping to 1.0.0

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+INSTALL_DIR=~/.local/share/applications/music-dir-player
+echo "Installing to $INSTALL_DIR"
+
+# The user downloaded our release package. Copy the entire directory into the user's home
+# Note, the mdp.desktop file will be found by the OS even though it is in a subdir of
+# ~/.local/share/applications
+rm -rf $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+cp -r ./* $INSTALL_DIR
+
+# Copy in the icon to the standard location
+cp ./mdp.png ~/.local/share/icons
+
+echo "All done. It will show up as Music Dir Player in your application list"
